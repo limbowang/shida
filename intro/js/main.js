dd7407aa872c0d4df789b794d4b90e23e8272b8f
limbowang <van8701@gmail.com>
2014-11-19 22:46:40 +0800

    Add No. 7~9 singer.

--- intro/js/main.js
+++ intro/js/main.js
@@ -213,7 +213,7 @@ $(window).load(function () {
                     var deltaX = movoX - startX;
                     moveX = startX = 0;
                     if (deltaX < -20) {
-                        if (index == 6) {
+                        if (index == 9) {
                             swipe.next();
                         } else {
                             slideIn();
